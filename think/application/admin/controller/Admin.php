b7242bb9d80a2063e982520b0cb95fd93cdc3b68
SeabiscuitHao <1017903523@qq.com>
2018-03-06 21:20:26 +0800

    2018.03.06

---
+++ think/application/admin/controller/Admin.php
@@ -0,0 +1,21 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+class Admin extends Controller
+{
+  public function lst()
+  {
+    return $this -> fetch('lst');
+  }
+
+  public function add()
+  {
+    return $this -> fetch('add');
+  }
+
+  public function edit()
+  {
+    return $this -> fetch('edit');
+  }
+
+}
